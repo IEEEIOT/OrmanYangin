97477ed24e24fa1b90847e347666bf72adf43905
Fırat <femresirvan@gmail.com>
2020-07-29 12:01:20 +0300

    router.js deki bug giderildi

--- routers/router.js
+++ routers/router.js
@@ -4,11 +4,11 @@ const router = express.Router();//
 const indexController = require('../controller/indexController');
 const notFoundController = require('../controller/notFoundController');
 const mqttDataController = require('../controller/mqttDataController');
-
+const historyController = require('../controller/historyController');
 
 router.get('/', indexController.renderIndex);
 // router.post('/getData', mqttDataController.getData);
-router.get('*', notFoundController.renderNotFound);
+// router.get('*', notFoundController.renderNotFound);
 router.get('/history', historyController.renderHistory);
 
 module.exports = router;
