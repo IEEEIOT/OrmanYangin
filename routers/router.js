1839bfd459930551d622fe4b923b43bf5f4a6bb1
Fırat <femresirvan@gmail.com>
2020-08-08 01:07:07 +0300

    API hazır vaziyete getirildi

--- routers/router.js
+++ routers/router.js
@@ -6,10 +6,14 @@ const notFoundController = require('../controller/notFoundController');
 const mqttDataController = require('../controller/mqttDataController');
 const historyController = require('../controller/historyController');
 const wsController = require('../controller/wsController');
+const apiController = require('../controller/apiController');
 
 router.get('/', indexController.renderIndex);
 // router.post('/getData', mqttDataController.getData);
 // router.get('*', notFoundController.renderNotFound);
 router.get('/history', historyController.renderHistory);
+router.get('/api/data', apiController.getData);
+router.get('/api/:number',apiController.getlastXData);
+router.get('/api/:id/:number', apiController.getlastxDatawithinId);
 
 module.exports = router;
