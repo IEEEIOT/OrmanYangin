4122e141a28660bf8d8066f2942e9c234755081e
femresirvan <60824063+femresirvan@users.noreply.github.com>
2020-09-14 01:32:24 +0300

    JWT işlemleri tamamlandı

--- routers/router.js
+++ routers/router.js
@@ -6,17 +6,19 @@ const notFoundController = require('../controller/notFoundController');
 // const realtimeDataController = require('../controller/realtimeDataController');
 const historyController = require('../controller/historyController');
 const apiController = require('../controller/apiController');
+const jwtController = require('../controller/jwtController');
 
 router.get('/', indexController.renderIndex);
 router.get('/',indexController.realtimedata);
 router.get('/history', historyController.renderHistory);
-router.get('/api/data', apiController.getData);
+// router.get('/api/',jwtController.checkAuth);
+router.get('/api/data',jwtController.checkAuth,apiController.getData);
 router.get('/api/number/:number',apiController.getlastXData);
 router.get('/api/id/:id/number/:number', apiController.getlastxDatawithinId);
 router.get('/api/yangin', apiController.getDangerData);
 router.get('/api/yangin/id/:id/number/:number' , apiController.getDangerData2);
 router.get('/api/send-data' , apiController.createData);
 router.get('/api/delete-data', apiController.deleteData);
-
+router.post('/api/getToken',jwtController.getToken);
 
 module.exports = router;
