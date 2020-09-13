3736c60e94141391852f5aa2dcb7bfb475a5ba78
femresirvan <60824063+femresirvan@users.noreply.github.com>
2020-09-13 19:10:06 +0300

    anlik veri gönderimi
    artık sayfa yenilenince de olucak

--- controller/indexController.js
+++ controller/indexController.js
@@ -1,3 +1,8 @@
-module.exports.renderIndex = (req, res) => {
+const db_operations = require('../lib/db_operations')
+
+module.exports.renderIndex = (req, res, next) => {
     res.render('indexView');
+    next();
 };
+
+module.exports.realtimedata = db_operations.anlikveriaramasi;
